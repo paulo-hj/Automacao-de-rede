fa31f66d941199201640c8701a489fc0993bb10f
Paulo Henrique <paulo.junior.ph@gmail.com>
2022-04-13 20:41:47 -0300

    corrigida função para verificar os sinais de todas as ONU

--- verificarSinalONU.py
+++ verificarSinalONU.py
@@ -1,7 +1,6 @@
 import telnetlib
 
 HOST = "10.50.50.50"
-
 tn = telnetlib.Telnet(HOST)
 tn.read_until(b":")
 tn.write(b"digistar\n")
@@ -10,15 +9,20 @@ tn.write(b"enable\n")
 
 tn.write(b"digistar\n")
 
-
-
 saida = tn.read_until(b'#').decode()
 print(saida)
 
-while i <= 10:
-    comando = "onu status 2/{}\n".format(i).encode()
-    tn.write(b""+comando)
-    i = i + 1
-    saida = tn.read_until(b'#').decode()
-    print(saida)
+def comandoOnu():
+    i = 1
+    j = 0
+    while i < 17:
+        comando = "onu status {}/{}\n".format(j,i).encode()
+        tn.write(b""+comando)
+        saida = tn.read_until(b'#').decode()
+        print(saida)
+        i = i + 1
+        if i == 17 and j < 8:
+            j = j + 1
+            i = 1
 
+comandoOnu()
