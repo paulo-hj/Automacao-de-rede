b85fe9a867bc47b2d47ed52001518da7978143ad
Paulo Henrique <paulo.junior.ph@gmail.com>
2022-04-13 21:58:43 -0300

    adicionado saída 'label' a tela de sinal da onu

--- verificarSinalONU.py
+++ verificarSinalONU.py
@@ -6,23 +6,27 @@ class Comandos():
     def conectar(self):
         HOST = "10.50.50.50"
         self.tn = telnetlib.Telnet(HOST)
+
     def login(self):
         self.tn.read_until(b":")
         self.tn.write(b"digistar\n")
         self.tn.write(b"enable\n")
         self.tn.write(b"digistar\n")
         saida = self.tn.read_until(b'#').decode()
+
         print(saida)
+
     def verificarSinal(self):
         porta = 0
         onu = 1
-        while onu < 17:
+        while onu < 3: #onu < 17:
             comando = "onu status {}/{}\n".format(porta,onu).encode()
             self.tn.write(b""+comando)
             saida = self.tn.read_until(b'#').decode()
-            print(saida)
+            self.primeiraOnu["text"] = "{}".format(str(saida))
+            #print(saida)
             onu = onu + 1
-            if onu == 17 and porta < 8:
+            if onu == 3 and porta < 2: #onu == 17 and porta < 8:
                 porta = porta + 1
                 onu = 1
 
@@ -38,14 +42,27 @@ class Interface():
         self.conectar()
         self.login()
         primeiraTela.mainloop()
+
     def widgetsTelaPrincipal(self):
         #Criação dos texto.
         Label(self.primeiraTela, text="Verificar sinal das ONU's").pack()
         #Criação dos botões.
-        botaoVerificarSinais = Button(self.primeiraTela, text="Verificar sinais das ONU", command=self.verificarSinal).pack()
+        Button(self.primeiraTela, text="Verificar sinais das ONU", command=self.telaSinal).pack()
         #Criação das saídas dos dados.
-    #def telaSinal(self):
 
+    def telaSinal(self):
+        segundaTela = Tk()
+        segundaTela.geometry("700x500+350+110")
+        segundaTela.title("Sinais das ONU's")
+        segundaTela.resizable(width=False, height=False)
+        self.segundaTela = segundaTela
+        self.widgetsTelaSinal()
+        self.verificarSinal()
+        segundaTela.mainloop()
+
+    def widgetsTelaSinal(self):
+        self.primeiraOnu = Label(self.segundaTela, text="", width=97, height=32)
+        self.primeiraOnu.pack()
 
 
 class Main(Comandos, Interface):
